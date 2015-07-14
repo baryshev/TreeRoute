919925fef02e60c0da707bb882e9d6607e98d31b
Rasmus Schultz <rasc@fynskemedier.dk>
2015-07-14 14:42:58 +0200

    bootstrap with composer autoloading

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,4 @@
 <?php
 // This is global bootstrap for autoloading
+
+require dirname(__DIR__) . '/vendor/autoload.php';
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,4 +1,2 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-
-require __DIR__ . '/../../src/Router.php';
